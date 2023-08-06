16e711e6b67ffdd0eab36e569fa67d16e2b4d966
wolfy-six <mendis.seneth@gmail.com>
2023-08-06 06:14:04 +0530

    need more fixing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import {BsFillTagFill} from 'react-icons/bs'
+import styles from '../styles'
 const Button = ({buttonText}) => {
   return (
-    <button className='button-bg rounded-md font-bold'>{buttonText}</button>
+    <button className={`${styles.flexCenter} button-bg rounded-md font-bold flex gap-2`}>{buttonText} <BsFillTagFill/> </button>
   )
 }
 
--- src/components/CrossPlatform.jsx
+++ src/components/CrossPlatform.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import styles, { layout } from "../styles";
-import {smartPhone} from '../assets'
+import { smartPhone, googlePlay, appleStore } from "../assets";
+import Button from "./Button";
 
 const CrossPlatform = () => {
   return (
     <section className={`${layout.section} `}>
       <div className={`${styles.flexStart} flex  flex-col flex-1`}>
-        <h1 className={`${styles.heading1} my-auto`}>
+        <h1 className={`${styles.heading1}`}>
           {" "}
           Cross platform <br className="sm:block hidden" />
           Task planner Do it.
         </h1>
 
-        <div className="w-full h-full  flex flex-col gap-6">
+        <div className={`w-full h-full  flex flex-col gap-6 `}>
           <div className="border-left mt-5">
             <h3 className="text-3xl tracking-wide">
               You can finally plane <br /> your tasks without pain.
             </h3>
-            <p className={`${styles.paragraph}`}>
+            <p
+              className={`py-4 font-normal tracking-wider text-justify md:text-left`}
+            >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit
               eum voluptatibus recusandae ex aspernatur quis quia, nemo quasi
               possimus officia.
@@ -26,14 +29,38 @@ const CrossPlatform = () => {
           <p className={`${styles.paragraph} border-left`}>
             Do not waste your time anymore
           </p>
-          <p className={`${styles.paragraph} border-left`}>
-            Be one Stop ahead
-          </p>
+          <p className={`${styles.paragraph} border-left`}>Be one Stop ahead</p>
+          <Button buttonText={"Get Started"} />
+          <div className="w-full flex justify-center sm:justify-start gap-6 mt-5">
+            <img
+              src={googlePlay}
+              alt=""
+              className=" w-[200px] h-[65px] aspect-auto"
+            />
+            <img
+              src={appleStore}
+              alt=""
+              className=" w-[200px] h-[65px] aspect-auto"
+            />
+          </div>
         </div>
       </div>
 
-      <div className={`${layout.sectionImg}`}>
-    <img src={smartPhone} alt="" />
+      <div className={`${layout.sectionImg} relative pt-20 md:pt-0`}>
+        <div className="w-[30%] h-[30%] top-0 right-0 black-gradient mx-auto backdrop-blur-lg absolute z-30  border-2 border-gray-700  rounded-xl  object-contain" />
+
+        <img
+          src={smartPhone}
+          alt=""
+          className="my-auto relative bottom-[100px] z-20"
+        />
+        <div className="w-[50%] h-[70%] black-gradient mx-auto backdrop-blur-lg absolute z-10  border-2 border-gray-700  rounded-xl  object-contain" />
+        <div
+          className={`pink__gradient w-[50%] h-[50%] right-0 top-0 absolute z-10`}
+        />
+        <div
+          className={`purple__gradient w-[50%] h-[40%] right-0 absolute z-0`}
+        />
       </div>
     </section>
   );
